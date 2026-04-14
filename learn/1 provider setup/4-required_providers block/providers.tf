4f0202aa7de2602e0c17e4fc8080668d39c94e22
Dama-27 <damathdesilva@gmail.com>
2026-04-14 23:57:34 +0530

    docs: add explanatory comments to required_providers block in providers.tf

--- learn/1 provider setup/4-required_providers block/providers.tf
+++ learn/1 provider setup/4-required_providers block/providers.tf
@@ -1,13 +1,24 @@
-# by the required_providers block you can define multiple providers with the specific hashicorp versions required
+# required_providers block
+
+# This is where you define:
+# - Which providers you use
+# - Which versions you want
+
 terraform {
   required_providers {
+
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.0, < 3.0"
     }
+
   }
 }
+
+# This helps avoid "it worked yesterday but not today" problems (It happens because of the new version releases)
+# Version control = stability
